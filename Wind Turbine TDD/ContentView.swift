4aa5289c1af329234d9bb3f63efdb4eb579d7b32
Resn4R <purview-joust.0x@icloud.com>
2023-11-16 18:38:15 +0000

    Initial Commit

---
+++ Wind Turbine TDD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wind Turbine TDD
+//
+//  Created by Vito Borghi on 16/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wind Turbine TDD/Wind_Turbine_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wind_Turbine_TDDApp.swift
+//  Wind Turbine TDD
+//
+//  Created by Vito Borghi on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wind_Turbine_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
